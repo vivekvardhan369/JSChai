b047ebb43abbe0a5f6b3fff48b4c377492631262
vivekvardhan369 <vivekvardhan.akula@gmail.com>
2024-06-13 01:46:58 +0530

    practice done

---
+++ 01.variables.js
@@ -0,0 +1,14 @@
+const accountId = 144556
+let accountEmail="vivelvarshan@gmail.com"
+var accountPassword="123450"
+accountCity ="Jaipur"
+
+// accountId = 2
+
+accountEmail="Vicky@gmail.com"
+accountPassword="42566"
+accountCity="HYD"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword]);
