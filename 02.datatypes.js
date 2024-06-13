675aa78a840bb7ac3b9bdbc1f50b546d3bd5d9b2
vivekvardhan369 <vivekvardhan.akula@gmail.com>
2024-06-13 18:54:56 +0530

    variables

---
+++ 02.datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readiability should be high
+console.log("vivek")
+
+let name="vivek"
+let age= 15
+let isLoggedIn = false
+
+//number ==> 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol =>unique
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
