2422376e87c438e4997998853ba88ce4f88b2cbf
vivekvardhan369 <vivekvardhan.akula@gmail.com>
2024-06-14 15:15:14 +0530

    conversionOP

---
+++ 03.conversionOpwration.js
@@ -0,0 +1,26 @@
+// let score= "vivek";
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueNumber=Number(score)
+// console.log(typeof valueNumber);
+// console.log( valueNumber);
+
+// "99" => 33
+//"33abc" =>NaN
+//true =>1; false =>0
+
+// let isLoggedIn=""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 =>false
+//"" => false
+// "vivek" =>true
+
+let someNumber= 44
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
