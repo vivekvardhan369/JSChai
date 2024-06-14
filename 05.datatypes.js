1ed7080edd71f7486b4dc370c9bd5221891092e3
vivekvardhan369 <vivekvardhan.akula@gmail.com>
2024-06-14 23:05:00 +0530

    datatypes

---
+++ 05.datatypes.js
@@ -0,0 +1,33 @@
+// Primitive
+
+// 7 types: String, Number , Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non Primitive)
+
+const score =100
+const scoreValue = 100.3
+
+const isLoggedIn=false
+const outsideTemp =null
+let userEmail;
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber= 454548454868948n
+
+// console.log(id ===anotherId);
+
+//Array, Objects, Functions 
+// const heros = ["shaktiman", "nagraj", "doga"]
+
+// let myObj{
+//     name:"vivek"
+//     age: 25
+// }
+
+// const myFunction = function(){
+//     console.log("hello world");
+// }
+
+console.log(typeof bigNumber);
