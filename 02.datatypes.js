ad28c66194be9c2da9ba05d44d7e0f47f51b8867
vivekvardhan369 <vivekvardhan.akula@gmail.com>
2024-06-14 22:33:14 +0530

    comparisions.js

--- 02.datatypes.js
+++ 02.datatypes.js
@@ -1,14 +1,12 @@
-"use strict"; //treat all JS code as newer version
+'use strict' //treat all JS code as newer version
 
 //alert("hello") // we are using nodejs, not browser
 
-console.log(3
-    +
-    3) //code readiability should be high
-console.log("vivek")
+console.log(3 + 3) //code readiability should be high
+console.log('vivek')
 
-let name="vivek"
-let age= 15
+let name = 'vivek'
+let age = 15
 let isLoggedIn = false
 
 //number ==> 2 to power 53
@@ -19,8 +17,7 @@ let isLoggedIn = false
 //undefined => value not assigned
 //symbol =>unique
 
-
 //object
 
-console.log(typeof undefined); //undefined
-console.log(typeof null); // object
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
--- 03.conversionOpwration.js
+++ 03.conversionOpwration.js
@@ -1,6 +1,5 @@
 // let score= "vivek";
 
-
 // console.log(typeof score);
 // console.log(typeof(score));
 
@@ -21,6 +20,40 @@
 //"" => false
 // "vivek" =>true
 
-let someNumber= 44
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let someNumber= 44
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+//**********************Operations **********/
+
+let value = 3;
+let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2= "vivek"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2+2);
+// console.log(1 + 2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 =num3 =2 + 2
+
+let gameCounter = 522
+
+gameCounter++
+console.log(gameCounter);
